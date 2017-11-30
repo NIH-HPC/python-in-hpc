65d0d3b80294dbadacd4010b2ea1f18f7333b5e6
Wolfgang Resch <wolfgang.resch@gmail.com>
2017-11-30 11:38:34 -0500

    simplified the vectorized numba implementation

--- profiling_and_optimizing/mandel09.py
+++ profiling_and_optimizing/mandel09.py
@@ -1,8 +1,8 @@
-from numba import jit, vectorize, guvectorize, float64, complex64, int32, float32
+from numba import jit, vectorize, complex64, int32
 import numpy as np
 
-@jit(int32(complex64, int32))
-def mandel9(c,maxiter):
+@vectorize([int32(complex64, int32)], target='parallel')
+def mandel9(c, maxiter):
     nreal = 0
     real = 0
     imag = 0
@@ -13,16 +13,10 @@ def mandel9(c,maxiter):
         if real * real + imag * imag > 4.0:
             return n
     return n
-
-@guvectorize([(complex64[:], int32[:], int32[:])], '(n),()->(n)',target='parallel')
-def mandel9v(c, maxit, output):
-    maxiter = maxit[0]
-    for i in range(c.shape[0]):
-        output[i] = mandel9(c[i],maxiter)
         
 def mandel_set9(xmin=-2.0, xmax=0.5, ymin=-1.25, ymax=1.25, width=1000, height=1000, maxiter=80):
     r1 = np.linspace(xmin, xmax, width, dtype=np.float32)
     r2 = np.linspace(ymin, ymax, height, dtype=np.float32)
     c = r1 + r2[:,None]*1j
-    n = mandel9v(c,maxiter)
+    n = mandel9(c,maxiter)
     return n
